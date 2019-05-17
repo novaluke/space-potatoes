ad7068b1f5dd5aa961c60e1ade79608db7efdb84
Luke Mayhew <luke@webuildlegends.com>
2019-05-17 09:21:15 -0600

    Finish setup

--- test/setup.tsx
+++ test/setup.tsx
@@ -2,20 +2,26 @@ import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 
 import { createMemoryHistory } from "history";
+import React from "react";
 import { Router } from "react-router-dom";
 import { render as rtlRender } from "react-testing-library";
 
-function render(
-  ui,
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+export const flushPromises = () =>
+  new Promise(resolve => setImmediate(resolve));
+
+export const render = (
+  ui: JSX.Element,
   {
     route = "/",
     history = createMemoryHistory({ initialEntries: [route] }),
     // tslint:disable-next-line:trailing-comma
     ...renderOptions
   } = {},
-) {
-  return {
-    ...rtlRender(<Router history={history}>{ui}</Router>, renderOptions),
-    history,
-  };
-}
+) => ({
+  ...rtlRender(<Router history={history}>{ui}</Router>, renderOptions),
+  history,
+});
