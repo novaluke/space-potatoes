b34ad024a7d411ec003a52d6102adbbdacd8f16b
Luke Mayhew <luke@webuildlegends.com>
2019-05-30 09:37:57 -0600

    Ship turns and moves up/down in response to player input

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,13 @@
-import { foldDyn, fromAnimationFrame, mapEvt, pipe } from "../frp";
-import { holdDyn } from "../frp/Dynamic";
+import {
+  Dynamic,
+  foldDyn,
+  fromAnimationFrame,
+  holdDyn,
+  mapEvt,
+  pipe,
+} from "../frp";
 import { drawPoint } from "../graphics/Geometry";
-import { isPressed, registerForKeyEvents } from "./keyboard";
+import { KEY_CODES, KEY_FLAGS, registerForKeyEvents } from "./keyboard";
 import { drawShip, Ship } from "./Ship";
 
 const drawBackground = (ctx: CanvasRenderingContext2D) => {
@@ -16,15 +22,40 @@ export default (ctx: CanvasRenderingContext2D) => {
     size: 20,
   };
 
-  const isThrusting = pipe(
-    mapEvt((keysPressed: number) => isPressed("w", keysPressed)),
-    holdDyn(false),
+  const turningModifier: Dynamic<-1 | 0 | 1> = pipe(
+    mapEvt((keysPressed: number) => {
+      const a = KEY_FLAGS[KEY_CODES.a];
+      const d = KEY_FLAGS[KEY_CODES.d];
+      if ((keysPressed & (a | d)) === d) {
+        return -1;
+      }
+      if ((keysPressed & (a | d)) === a) {
+        return 1;
+      }
+      return 0;
+    }),
+    holdDyn(0 as -1 | 0 | 1),
+  )(registerForKeyEvents());
+
+  const thrustModifier: Dynamic<-1 | 0 | 1> = pipe(
+    mapEvt((keysPressed: number) => {
+      const w = KEY_FLAGS[KEY_CODES.w];
+      const s = KEY_FLAGS[KEY_CODES.s];
+      if ((keysPressed & (w | s)) === w) {
+        return -1;
+      }
+      if ((keysPressed & (w | s)) === s) {
+        return 1;
+      }
+      return 0;
+    }),
+    holdDyn(0 as -1 | 0 | 1),
   )(registerForKeyEvents());
 
   const updateShip = (ship: Ship): Ship => ({
     ...ship,
-    angle: ship.angle + (1 / 180) * Math.PI,
-    pos: [ship.pos[0], isThrusting.value ? ship.pos[1] - 2 : ship.pos[1]],
+    angle: ship.angle + (1 / 180) * Math.PI * turningModifier.value,
+    pos: [ship.pos[0], ship.pos[1] + 2 * thrustModifier.value],
   });
 
   const render = (ship: Ship) => {
