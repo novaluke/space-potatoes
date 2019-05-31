801cfd97c8dbee3b757940ccce17c05a86e25228
Luke Mayhew <luke@webuildlegends.com>
2019-05-31 09:44:59 -0600

    Ship update logic uses functional composition, ship accelerates

--- src/game/Ship.ts
+++ src/game/Ship.ts
@@ -1,9 +1,13 @@
-import { Point } from "../graphics/Geometry";
+import { Dynamic, Event, foldDyn, pipe } from "../frp";
+import { attach } from "../frp/Event";
+import { Point, vectorXY } from "../graphics/Geometry";
+import { KEY_CODES, KEY_FLAGS } from "./keyboard";
 
 export interface Ship {
   pos: Point;
   size: number;
   angle: number;
+  vel: [number, number]; // [x, y]
 }
 
 interface ShipBounds {
@@ -14,9 +18,9 @@ interface ShipBounds {
 
 // Dimensions such that the center point is the triangle's centroid.
 const shipBounds: ShipBounds = {
-  backLeft: [15, 10],
-  backRight: [-15, 10],
-  nose: [0, -20],
+  backLeft: [-10, -15],
+  backRight: [-10, 15],
+  nose: [20, 0],
 };
 
 export const drawShip = (ctx: CanvasRenderingContext2D) => {
@@ -32,3 +36,60 @@ export const drawShip = (ctx: CanvasRenderingContext2D) => {
   ctx.lineWidth = 1;
   ctx.stroke();
 };
+
+type ShipUpdate = (
+  timeDelta: number,
+  keysPressed: number,
+) => (prevShip: Ship) => Ship;
+
+export const updateShip = (
+  timeDelta: Event<number>,
+  keysPressed: Dynamic<number>,
+  starterShip: Ship,
+  // tslint:disable-next-line:trailing-comma
+  ...updates: ShipUpdate[]
+): Dynamic<Ship> =>
+  foldDyn(
+    (oldShip, [keyMask, delta]: [number, number]) =>
+      pipe(...updates.map(update => update(delta, keyMask)))(oldShip),
+    starterShip,
+  )(attach(keysPressed, timeDelta));
+
+// turnRate = degrees/ms (ie. give it (degrees/s) / 1000)
+export const updateAngle = (turnRate: number): ShipUpdate => (
+  timeDelta,
+  keysPressed,
+) => prevShip => {
+  const a = KEY_FLAGS[KEY_CODES.a];
+  const d = KEY_FLAGS[KEY_CODES.d];
+  const directionKeysPressed = keysPressed & (a | d);
+  const direction =
+    directionKeysPressed === a ? -1 : directionKeysPressed === d ? 1 : 0;
+  return {
+    ...prevShip,
+    angle: prevShip.angle + (turnRate / 180) * Math.PI * direction * timeDelta,
+  };
+};
+
+// acceleration = px/ms^2 (ie. give it (px/s^2) / 1000)
+export const updateVel = (acceleration: number): ShipUpdate => (
+  timeDelta,
+  keysPressed,
+) => prevShip => {
+  const [accelX, accelY] = vectorXY(
+    keysPressed & KEY_FLAGS[KEY_CODES.w] ? acceleration * timeDelta : 0,
+    prevShip.angle,
+  );
+  return {
+    ...prevShip,
+    vel: [prevShip.vel[0] + accelX, prevShip.vel[1] + accelY],
+  };
+};
+
+export const updatePos: ShipUpdate = timeDelta => prevShip => ({
+  ...prevShip,
+  pos: [
+    prevShip.pos[0] + prevShip.vel[0] * timeDelta,
+    prevShip.pos[1] + prevShip.vel[1] * timeDelta,
+  ],
+});
--- src/game/main.ts
+++ src/game/main.ts
@@ -1,14 +1,14 @@
-import {
-  Dynamic,
-  foldDyn,
-  fromAnimationFrame,
-  holdDyn,
-  mapEvt,
-  pipe,
-} from "../frp";
+import { fromAnimationFrame } from "../frp";
 import { drawPoint, Transforms, withContext } from "../graphics/Geometry";
-import { KEY_CODES, KEY_FLAGS, registerForKeyEvents } from "./keyboard";
-import { drawShip, Ship } from "./Ship";
+import { registerForKeyEvents } from "./keyboard";
+import {
+  drawShip,
+  Ship,
+  updateAngle,
+  updatePos,
+  updateShip,
+  updateVel,
+} from "./Ship";
 
 const drawBackground = (ctx: CanvasRenderingContext2D) => {
   ctx.fillStyle = "black";
@@ -17,46 +17,23 @@ const drawBackground = (ctx: CanvasRenderingContext2D) => {
 
 export default (ctx: CanvasRenderingContext2D) => {
   const starterShip: Ship = {
-    angle: 0,
+    angle: (-90 / 180) * Math.PI,
     pos: [ctx.canvas.width / 2, ctx.canvas.height / 2],
     size: 20,
+    vel: [0, 0],
   };
 
-  const turningModifier: Dynamic<-1 | 0 | 1> = pipe(
-    mapEvt((keysPressed: number) => {
-      const a = KEY_FLAGS[KEY_CODES.a];
-      const d = KEY_FLAGS[KEY_CODES.d];
-      if ((keysPressed & (a | d)) === a) {
-        return -1;
-      }
-      if ((keysPressed & (a | d)) === d) {
-        return 1;
-      }
-      return 0;
-    }),
-    holdDyn(0 as -1 | 0 | 1),
-  )(registerForKeyEvents());
+  const fpsDelta = fromAnimationFrame();
+  const keysPressed = registerForKeyEvents();
 
-  const thrustModifier: Dynamic<-1 | 0 | 1> = pipe(
-    mapEvt((keysPressed: number) => {
-      const w = KEY_FLAGS[KEY_CODES.w];
-      const s = KEY_FLAGS[KEY_CODES.s];
-      if ((keysPressed & (w | s)) === w) {
-        return -1;
-      }
-      if ((keysPressed & (w | s)) === s) {
-        return 1;
-      }
-      return 0;
-    }),
-    holdDyn(0 as -1 | 0 | 1),
-  )(registerForKeyEvents());
-
-  const updateShip = (ship: Ship): Ship => ({
-    ...ship,
-    angle: ship.angle + (1 / 180) * Math.PI * turningModifier.value,
-    pos: [ship.pos[0], ship.pos[1] + 2 * thrustModifier.value],
-  });
+  const dynShip = updateShip(
+    fpsDelta,
+    keysPressed,
+    starterShip,
+    updateAngle(180 / 1000),
+    updateVel(0.25 / 1000),
+    updatePos,
+  );
 
   const render = (ship: Ship) => {
     drawBackground(ctx);
@@ -68,5 +45,5 @@ export default (ctx: CanvasRenderingContext2D) => {
     withContext(ctx, shipRelative)(drawPoint("red"));
   };
 
-  foldDyn(updateShip, starterShip)(fromAnimationFrame()).subscribe(render);
+  dynShip.subscribe(render);
 };
