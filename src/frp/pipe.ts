92753687e970a7d299a51331326592979304d02b
Luke Mayhew <luke@webuildlegends.com>
2019-06-03 16:20:32 -0600

    Improve `pipe` type inference via overloads instead of Pipe type

--- src/frp/pipe.ts
+++ src/frp/pipe.ts
@@ -1,4 +1,34 @@
-import { Pipe } from "ts-functionaltypes";
-
-export const pipe: Pipe = (...fns) => input =>
-  fns.reduce((val, fn) => fn(val), input);
+export function pipe<Args extends any[], A, B>(
+  fn1: (...args: Args) => A,
+  fn2: (val: A) => B,
+): (...args: Args) => B;
+export function pipe<Args extends any[], A, B, C>(
+  fn1: (...args: Args) => A,
+  fn2: (val: A) => B,
+  fn3: (val: B) => C,
+): (...args: Args) => C;
+export function pipe<Args extends any[], A, B, C, D>(
+  fn1: (...args: Args) => A,
+  fn2: (val: A) => B,
+  fn3: (val: B) => C,
+  fn4: (val: C) => D,
+): (...args: Args) => D;
+export function pipe<Args extends any[], A, B, C, D, E>(
+  fn1: (...args: Args) => A,
+  fn2: (val: A) => B,
+  fn3: (val: B) => C,
+  fn4: (val: C) => D,
+  fn5: (val: D) => E,
+): (...args: Args) => E;
+export function pipe<Args extends any[], A, B, C, D, E, F>(
+  fn1: (...args: Args) => A,
+  fn2: (val: A) => B,
+  fn3: (val: B) => C,
+  fn4: (val: C) => D,
+  fn5: (val: D) => E,
+  fn6: (val: E) => F,
+): (...args: Args) => F;
+export function pipe<T>(...fns: Array<(val: T) => T>): (val: T) => T;
+export function pipe(...fns: any[]) {
+  return (input: any) => fns.reduce((val, fn) => fn(val), input);
+}
